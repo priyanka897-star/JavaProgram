7d7dbdb77b808865d3962c2a6e0e90b1c21d57fb
priyanka897-star <priyankamore8997@gmail.com>
2021-09-25 08:43:32 +0530

    Added:Calculate Employee Wage using Switch Case

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -2,25 +2,31 @@ package javapractice;
 
 public class EmpWageBuilder {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		
 		int isFullTime = 1;
-		int isPartTime=0;
-		double empCheck = Math.floor(Math.random()*10)%3;
-		if (empCheck == isFullTime) {
-			int empRatePerHr = 200;
-		    int empHrs = 8;
-		    int  salary = ( empHrs * empRatePerHr );
-		    System.out.println("Daily Wage of Full Time Employee is : "+salary);
-			
+		int isPartTime = 2;
+		
+		int EmpRatePerHr = 20;
+		int empHrs =0;
+		int empWage =0;
+		
+		double empCheck = Math.floor(Math.random() *10 %3);
+		int c = (int)empCheck;
+		switch(c)	{  
+		case 1 :
+		 
+				empHrs = 8;
+				empWage = empHrs * EmpRatePerHr;
+		break;
+		case 2:
+		 
+				empHrs = 4;
+		 empWage = empHrs * EmpRatePerHr;
+		 break;
+		 default:
+			empHrs = 0;
 		}
-		else if (empCheck == isPartTime) {
-			
-			int empRatePerHr = 200;
-		    int empHrs = 4;
-		    int  salary = ( empHrs * empRatePerHr );
-			System.out.println(" Daily Wage of Part Time Employee is :"+salary);
-			
+		System.out.println(empWage);
 		}
 	}
-
-}
+		
