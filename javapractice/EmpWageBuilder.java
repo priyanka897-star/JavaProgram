d22a450d3d96979bc29533331b898a7c5250626e
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-09-25 16:43:38 +0530

    Added:Functionality to print Welcome message

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -3,30 +3,7 @@ package javapractice;
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 		
-		int isFullTime = 1;
-		int isPartTime = 2;
-		
-		int EmpRatePerHr = 20;
-		int empHrs =0;
-		int empWage =0;
-		
-		double empCheck = Math.floor(Math.random() *10 %3);
-		int c = (int)empCheck;
-		switch(c)	{  
-		case 1 :
-		 
-				empHrs = 8;
-				empWage = empHrs * EmpRatePerHr;
-		break;
-		case 2:
-		 
-				empHrs = 4;
-		 empWage = empHrs * EmpRatePerHr;
-		 break;
-		 default:
-			empHrs = 0;
-		}
-		System.out.println("Daily Employee Wage:"+empWage);
-		}
+		System.out.println("Welcome to Employee Wage Computation");
 	}
+}	
 		
