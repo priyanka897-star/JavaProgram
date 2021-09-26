973eb87d36f9f3055cbfa65ddf6972deeab6c876
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 20:28:58 +0530

    Added:Code to Calculate Daily Employee Wage

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -3,20 +3,23 @@ package javapractice;
 public class EmpWageBuilder {
 	
 	public static void main(String[] args) {
-		
-				//Constants
-			int IS_FULL_Time = 1;
-				//Computation
-			double empCheck = Math.floor(Math.random() *10 %2);
-			if (empCheck == IS_FULL_Time) {
-				System.out.println("Employee is Present");
-			}	
-			else {
-				System.out.println("Employee is Absent");
+		final int IS_FULL_TIME = 1;
+		final int EMP_RATE_PER_HOUR = 20;
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random() *10 %2);
+		if (empCheck == IS_FULL_TIME) {
+				empHrs = 8;
+		}		
+		else {
+			    empHrs = 0;
 		}
-				
-		
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Daily Employee Wage: " + empWage);
 	}
+		
+	
 		
 		
 }	
