fb0b9abc1e4bfd0945e0588d6ad57509f49011e2
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 20:12:19 +0530

    Added:Code to check Employee Attendance

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -3,36 +3,20 @@ package javapractice;
 public class EmpWageBuilder {
 	
 	public static void main(String[] args) {
-		//Variable
-		 final int EMP_FULL_TIME = 1;
-		 final int EMP_PART_TIME = 2;
-		 final int EMP_RATE_PER_HOUR = 20;
-		 final int NUM_OF_WORKING_DAYS=20;
-		 final int MAX_HRS_IN_MONTH = 100;
-		  int totalEmpHrs = 0,totalEmpWage = 0,totalWorkingDays = 0;
-		  while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-		  {
-			  int empHrs;
-			  totalWorkingDays++;
-			  double empCheck = Math.floor(Math.random() * 10) % 3;
-			  int i = (int)empCheck;
-			  switch(i) {
-			  case EMP_FULL_TIME:
-				  empHrs = 8;
-				  break;
-			  case EMP_PART_TIME:
-				  empHrs = 4;
-				  break;
-			 default:
-				 empHrs = 0;
-			    }
-	            totalEmpHrs = totalEmpHrs + empHrs;
-	            totalEmpWage = EMP_RATE_PER_HOUR * totalEmpHrs;
-		        System.out.println(" Day:" + totalWorkingDays + "Employee Hours:" +empHrs +"Employee Wage:" +totalEmpWage );
-		  }
-		  totalEmpWage = EMP_RATE_PER_HOUR * totalEmpHrs;
-		  System.out.println( "Total Employee Wage:" +totalEmpWage);
-	  
-		  
+		
+				//Constants
+			int IS_FULL_Time = 1;
+				//Computation
+			double empCheck = Math.floor(Math.random() *10 %2);
+			if (empCheck == IS_FULL_Time) {
+				System.out.println("Employee is Present");
+			}	
+			else {
+				System.out.println("Employee is Absent");
+		}
+				
+		
 	}
+		
+		
 }	
