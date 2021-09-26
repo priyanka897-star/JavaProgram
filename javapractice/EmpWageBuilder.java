9d2116a49d2575a63f060f64c503f689dd92d5f3
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 20:40:01 +0530

    Added:code to Add Part time Employee ,Calculate Employee Wage

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -3,23 +3,19 @@ package javapractice;
 public class EmpWageBuilder {
 	
 	public static void main(String[] args) {
-		final int IS_FULL_TIME = 1;
+		final int EMP_FULL_TIME = 1;
+		final int EMP_PART_TIME = 2;
 		final int EMP_RATE_PER_HOUR = 20;
-		//Variables
-		int empHrs = 0;
-		int empWage = 0;
-		double empCheck = Math.floor(Math.random() *10 %2);
-		if (empCheck == IS_FULL_TIME) {
-				empHrs = 8;
-		}		
-		else {
-			    empHrs = 0;
-		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Daily Employee Wage: " + empWage);
+		int empWorkingHour;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+	    if(empCheck == EMP_FULL_TIME) 
+			empWorkingHour = 8; 
+		else if(empCheck == EMP_PART_TIME)
+		    empWorkingHour = 4;
+		else
+		    empWorkingHour = 0;
+	    int salary =EMP_RATE_PER_HOUR  * empWorkingHour;   
+	    System.out.println(" Employee Salary is :" + salary);	
 	}
-		
-	
-		
-		
+				
 }	
