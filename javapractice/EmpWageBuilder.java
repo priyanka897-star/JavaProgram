14681c8fbe2e8564d410b675802e5806c3f3e918
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 13:02:46 +0530

    Added:Functionality to add Number of day for Month,calculate Employee Wage

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -1,32 +1,38 @@
 package javapractice;
 
 public class EmpWageBuilder {
+	
 	public static void main(String[] args) {
+		//Variable
+		final int EMP_FULL_TIME = 1;
+		final int EMP_PART_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
+		final int NUM_OF_WORKING_DAYS = 20;
+		int empHours = 0,empWage = 0,totalEmpWage = 0;
+		//Computation
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			
+            //computation
 		
-		int isFullTime = 1;
-		int isPartTime = 2;
-		
-		int EmpRatePerHr = 20;
-		int empHrs =0;
-		int empWage =0;
-		
-		double empCheck = Math.floor(Math.random() *10 %3);
-		int c = (int)empCheck;
-		switch(c)	{  
-		case 1 :
-		 
-				empHrs = 8;
-				empWage = empHrs * EmpRatePerHr;
-		break;
-		case 2:
-		 
-				empHrs = 4;
-		 empWage = empHrs * EmpRatePerHr;
-		 break;
-		 default:
-			empHrs = 0;
+		  int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		  int i=(int)empCheck;
+		  switch(i) {
+		  case EMP_FULL_TIME:
+			empHours = 8;
+			break;
+		  case EMP_PART_TIME:
+			empHours = 4;
+			break;
+		  default:
+			empHours = 0;
 		}
-		System.out.println("Daily Employee Wage:"+empWage);
+       
+	    empWage = empHours  * EMP_RATE_PER_HOUR;
+        totalEmpWage = totalEmpWage + empWage;
+	    System.out.println(" Employee Wage for day :"+ day + " is :" + empWage);
+	    System.out.println("Total Employee Wage:"+totalEmpWage);
 		}
+		
 	}
-		
+				
+}
