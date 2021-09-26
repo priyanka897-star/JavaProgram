84cfc33c89a3e3c3bd1a17162700e4b1e7924a88
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 20:50:18 +0530

    Added:Code to Calculate Employee Wage using Case Statement

--- javapractice/EmpWageBuilder.java
+++ javapractice/EmpWageBuilder.java
@@ -1,21 +1,27 @@
 package javapractice;
 
 public class EmpWageBuilder {
-	
 	public static void main(String[] args) {
-		final int EMP_FULL_TIME = 1;
-		final int EMP_PART_TIME = 2;
-		final int EMP_RATE_PER_HOUR = 20;
-		int empWorkingHour;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-	    if(empCheck == EMP_FULL_TIME) 
-			empWorkingHour = 8; 
-		else if(empCheck == EMP_PART_TIME)
-		    empWorkingHour = 4;
-		else
-		    empWorkingHour = 0;
-	    int salary =EMP_RATE_PER_HOUR  * empWorkingHour;   
-	    System.out.println(" Employee Salary is :" + salary);	
+		
+	     final int EMP_FULL_TIME = 1;
+	     final int EMP_PART_TIME = 2;
+	     final int EMP_RATE_PER_HOUR = 20;
+	     int empHours = 0;
+	     int empWage  = 0;
+	    //computation
+	
+	    int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	    switch(empCheck) {
+	    case EMP_FULL_TIME:
+		      empHours = 8;
+		      break;
+	    case EMP_PART_TIME:
+		      empHours= 4;
+		       break;
+	    default:
+		      empHours = 0;
 	}
-				
+    empWage = empHours  * EMP_RATE_PER_HOUR;
+    System.out.println(" Employee Wage is :" + empWage);	
+  }	
 }	
